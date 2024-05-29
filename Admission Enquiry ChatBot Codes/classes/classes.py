1c3368a09c8a6825a9e4b8e1bf0ffaf5762b07da
Megha Bhat <meghajbhat@gmail.com>
2024-05-29 13:59:12 +0530

    first commit

---
+++ Admission Enquiry ChatBot Codes/classes/classes.py
@@ -0,0 +1,16 @@
+#NAME : MEGHA BHAT
+#EMAIL : meghajbhat@gmail.com
+
+import pickle
+
+# Example data
+classes = ['Location', 'classes', 'courseDuration', 'courses', 'exams', 'facilities', 'fee', 'funActivities', 'goodbye', 'greetings', 'hours', 'invalid', 'name', 'semDuration', 'semesters', 'studentRequirements', 'teachingStyle', 'thanks']
+
+# Path to the PKL file
+file_path = 'classes.pkl'
+
+# Writing the data to a PKL file
+with open(file_path, 'wb') as file:
+    pickle.dump(classes, file)
+
+print(f"Data has been written to {file_path}")
---
+++ Admission Enquiry ChatBot Codes/words/see_words_pkl_file.py
@@ -0,0 +1,12 @@
+#NAME : MEGHA BHAT
+#EMAIL : meghajbhat@gmail.com
+
+import pickle
+
+# Open the PKL file in read-binary mode
+with open('words.pkl', 'rb') as file:
+    # Load the contents of the file
+    data = pickle.load(file)
+
+# Print the data to verify
+print(data)
